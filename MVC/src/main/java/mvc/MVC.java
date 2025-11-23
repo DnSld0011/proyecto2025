c3035df26e362b042d95e2b79a14ffdbfd3aa770
DnSld0011 <gemi290926@gmail.com>
2025-11-23 11:47:49 -0500

    he cambiado el sout del mensaje de salida

--- MVC/src/main/java/mvc/MVC.java
+++ MVC/src/main/java/mvc/MVC.java
@@ -3,6 +3,6 @@ package mvc;
 public class MVC {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("BIENVENIDO A LIMA");
     }
 }
